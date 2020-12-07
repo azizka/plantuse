cea63ccd1a516e3c65db98fb09d52955efd4a847
Alexander Zizka <zizka.alexander@gmail.com>
2020-12-07 16:48:25 +0100

    first package version

---
+++ R/count_organs.R
@@ -0,0 +1,28 @@
+#'Title Counting Plant Organs
+#'
+#'THis function counts the numer of plant organs used. 
+#'
+#'@param x This is a data.frame or tibble of plant uses. 
+#'needs to include columns "organ.rough".
+#'
+#'@export
+#'@examples
+#'
+#'data("plant_use")
+#'
+#'count_organs(x = plant_use)
+#'
+#'@importFrom dplyr count group_by
+#'
+
+
+count_organs <- function(x){
+  dat <- x %>% 
+    group_by(organ.rough) %>% 
+    count()
+  
+  ggplot(data = dat)+
+    geom_bar(aes(x = organ.rough, y = n), stat = "identity")+
+    theme_bw()+
+    theme(axis.text = element_text(angle = 90))
+}
---
+++ R/plantuse-package.R
@@ -0,0 +1,25 @@
+
+#'plant_use
+#' 
+#' A dataset of plant uses.
+#' 
+#' @format A data with 7 variables:
+#' \describe{
+#' \item{accepted.name}{character. The accepted taxonomic name of the species}
+#' \item{family}{the plant family name}
+#' }
+#' 
+#' @examples
+#' 
+#' data("plant_use")
+#'
+"plant_use"
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
